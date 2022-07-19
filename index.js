f55164f0bc8530d84f7220221852971d739d252b
joanneks <joanne.kan@trentglobal.edu.sg>
2022-07-19 09:44:25 +0000

    Added many files

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
 const wax = require('wax-on');
+require('handlebars-helpers')({
+    'handlebars': hbs.handlebars
+})
 require('dotenv').config()
 const mysql2 = require('mysql2/promise'); //use await/async,we must use the promise version of mysql2
 const app = express();
@@ -65,11 +68,244 @@ async function main(){
         }
         console.log(query,bindings)
         // 3 types of cyber security attacks : sql injections, xss (cross site scripting), csrf(cross server request forgery)
-
+        //prepared statement prevents against sql injections
+        //connection executes returns an array of results
         let [actors] = await connection.execute(query,bindings);
         res.render('search',{
             'actors':actors
         })
+    })
+
+
+    app.get('/actors/create', async function(req,res){
+        res.render('create_actor');
+    })
+
+    app.post('/actors/create', async function(req,res){
+        const query = "insert into actor (first_name, last_name) values (?, ?)";
+        const bindings = [req.body.first_name, req.body.last_name];
+        await connection.execute(query, bindings);
+        res.redirect('/actors');
+    })
+
+    app.get('/actors/:actor_id/update', async function (req, res) {
+        const actorId = parseInt(req.params.actor_id);
+        const query = "select * from actor where actor_id = ?";
+        const [actors] = await connection.execute(query, [actorId]);
+        const actorToUpdate = actors[0]; // since we are only expecting one result,we just take the first index
+        res.render('update_actor',{
+            'actor': actorToUpdate
+        })
+    })
+    app.post('/actors/:actor_id/update', async function(req,res){
+
+        if (req.body.first_name.length > 45 || req.body.last_name > 45) {
+            res.status(400);
+            res.send("Invalid request");
+            return;
+        }
+
+        // sample query
+        // update actor set first_name="Zoe2", last_name="Tay2" WHERE actor_id = 202;
+        const query = `update actor set first_name=?, last_name=? WHERE actor_id = ?`
+        const bindings = [req.body.first_name, req.body.last_name, parseInt(req.params.actor_id)];
+        await connection.execute(query, bindings);
+        res.redirect('/actors');
+    })
+
+    app.post ('/actors/:actor_id/delete', async function(req,res){
+        const query = "DELETE FROM actor WHERE actor_id = ?";
+        const bindings = [parseInt(req.params.actor_id)];
+        await connection.execute(query,bindings);
+        res.redirect('/actors');
+    })
+    // app.get('/category',async function(req,res){
+    //     const [category] = await connection.execute("SELECT * from category ORDER BY name ASC");
+    //     res.render('category',{
+    //         category
+    //     })
+    // })
+
+    app.get('/category',async function(req,res){
+
+            let query = "SELECT * from category where 1";
+            bindings = []
+            if (req.query.name){
+                query += ` AND name LIKE ?`
+                bindings.push("%"+req.query.name+"%")
+            }
+            if(req.query.nameOrder==="Name Ascending"){
+                query += `ORDER BY name ?`
+                bindings.push("ASC")
+            }
+            if(req.query.nameOrder==="Name Descending"){
+                query += `ORDER BY name ?`
+                bindings.push("DESC")
+            }
+            const [category] = await connection.execute(query,bindings);
+            console.log(query,bindings)
+            res.render('category',{
+                category
+            })
+    })
+    
+    // app.post('/category',async function(req,res){
+    //     const query = "SELECT * from category name LIKE ? ORDER BY name ASC";
+    //     bindings = [req.body.name]
+    //     const [category] = await connection.execute(query,bindings);
+        
+    //     res.render('category',{
+    //         category
+    //     })
+    // })
+
+    app.get('/category/create',async function(req,res){
+        res.render('category_create');
+    })
+    app.post('/category/create',async function(req,res){
+        let categoryName =req.body.name;
+            if (categoryName.length<=25){
+                const query = `insert into category (name) values (?)`;
+                const bindings = [req.body.name];
+                await connection.execute(query,bindings);
+                res.redirect('/category');
+            } else{
+                res.status(400);
+                res.send("Invalid Request")
+            }
+    })
+    app.get('/category/:category_id/update',async function(req,res){
+        categoryId = req.params.category_id;
+        const query = `SELECT * from category WHERE category_id = ?`;
+        [category] = await connection.execute(query,[categoryId]);
+        categoryToUpdate = category[0]
+        res.render('category_update',{
+            'category':categoryToUpdate
+        })
+    })
+    app.post('/category/:category_id/update',async function(req,res){
+        categoryId = req.params.category_id;
+        const query = `UPDATE category set name=? WHERE category_id = ?`;
+        bindings = [req.body.name,categoryId]
+        await connection.execute(query,bindings);
+        res.redirect('/category');
+    })
+    app.get('/category/:category_id/delete',async function(req,res){
+        categoryId = req.params.category_id;
+        const query = `SELECT * from category WHERE category_id = ?`;
+        [category] = await connection.execute(query,[categoryId]);
+        categoryToUpdate = category[0]
+        res.render('category_delete',{
+            'category':categoryToUpdate
+        })
+    })
+    app.post('/category/:category_id/delete',async function(req,res){
+        // find all films which cateegory_id equal to the one that we try to delete
+        const deleteQuery = "DELETE FROM film_category where category_id= ?";
+        await connection.execute(deleteQuery, [parseInt(req.params.category_id)])
+
+        categoryId = req.params.category_id;
+        const query = `DELETE FROM category WHERE category_id=?`
+        const bindings = [categoryId]
+        await connection.execute(query,bindings);
+        res.redirect('/category');
+    })
+
+    
+    app.get('/films/create', async function (req, res) {
+        const [languages] = await connection.execute(
+            "SELECT * FROM language"
+        );
+
+        res.render('create_film', {
+            languages: languages
+        })
+    })
+
+    
+    app.get('/categories/:category_id/delete', async function (req, res) {
+        const query = "SELECT * from category where category_id = ?"
+        const [categories] = await connection.execute(query, [parseInt(req.params.category_id)]);
+        const categoryToDelete = categories[0];
+        res.render('confirm_delete_category', {
+            'category': categoryToDelete
+        })
+    })
+
+    app.post('/categories/:category_id/delete', async function (req, res) {
+
+        // find all the films which have category_id equal to the one that we are trying to delete
+        const deleteQuery = "DELETE FROM film_category where category_id = ?";
+        await connection.execute(deleteQuery, [parseInt(req.params.category_id)]);
+
+        const query = "DELETE FROM category where category_id = ?";
+        const bindings = [parseInt(req.params.category_id)];
+        await connection.execute(query, bindings);
+        res.redirect('/categories');
+    })
+
+    
+    app.get('/films/create', async function (req, res) {
+        const [languages] = await connection.execute(
+            "SELECT * FROM language"
+        );
+
+        res.render('create_film', {
+            languages: languages
+        })
+    })
+
+    app.get('/films', async function (req, res) {
+        const [films] = await connection.execute(`SELECT film_id, title, description, language.name AS 'language' FROM film 
+                                                  JOIN language 
+                                                  ON film.language_id = language.language_id`);
+
+        console.log(films);
+        res.render('films', {
+            films: films
+        })
+    })
+
+    app.post('/films/create', async function (req, res) {
+        /*
+        insert into film (title, description, language_id)
+               values ("The Lord of the Ring", "blah blah blah", 1);
+        */
+        await connection.execute(
+            `insert into film (title, description, language_id) values (?, ?, ?)`,
+            [req.body.title, req.body.description, parseInt(req.body.language_id), ]
+        );
+        res.redirect('/films');
+    })
+
+    app.get('/films/:film_id/update', async function (req, res) {
+        const [languages] = await connection.execute("SELECT * from language");
+        const [films] = await connection.execute("SELECT * from film where film_id = ?",
+            [parseInt(req.params.film_id)]);
+        const filmToUpdate = films[0];
+        res.render('update_film',{
+            'film':filmToUpdate,
+            'languages':languages
+        })
+
+    })
+
+    
+    app.post('/films/:film_id/update', async function(req,res){
+        /* sample query:
+         update film set title="ASD ASD",
+                 description="ASD2 ASD2",
+                 language_id=3
+                WHERE film_id=1;
+        */
+       await connection.execute(
+        `UPDATE film SET title=?,
+                         description=?,
+                         language_id=?
+                         WHERE film_id=?`,
+        [req.body.title, req.body.description, parseInt(req.body.language_id), req.params.film_id]
+       );
+       res.redirect('/films');
     })
 };
 
